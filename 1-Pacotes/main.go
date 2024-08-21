bac90b19e90355c1e3c0f85ee1fe046338a45e88
Guilherme Gaspar <golhiergaspar@gmail.com>
2024-08-20 21:34:54 -0300

    remoção

--- 1-Pacotes/main.go
+++ 1-Pacotes/main.go
@@ -3,14 +3,9 @@ package main
 import (
 	"fmt"
 	"modulo/auxiliar"
-
-	"github.com/badoux/checkmail"
 )
 
 func main() {
 	fmt.Println("escrevendo main")
 	auxiliar.Escrever()
-
-	erro := checkmail.ValidateFormat("devbook@gmail.com")
-	fmt.Println(erro)
 }
